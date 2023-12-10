467bab9e5bdedceaf95a4c3016b5211f330c12d8
Samuel Kong <samuel.kong@auros.global>
2023-12-10 23:51:08 +0800

    get references llm

--- find_references.py
+++ find_references.py
@@ -11,18 +11,19 @@ def find_references(input1: str) -> str:
     references = re.findall(r'\[(\d+)\]\s*(.*?)\n(?=\[\d{1,3}\]|\Z)', reference_text, re.DOTALL)
     for reference in references:
         ref_number = int(reference[0].strip())
-        ref_text = reference[1].strip().replace('\n', '').rstrip('.')
-        #replace ibid with source from previous number
+        ref_text = reference[1].strip().replace('\n', '').removesuffix('.')
+        
+        # Replace ibid with source from previous number
         if 'ibid' in ref_text.lower():
             prev_ref_text =  reference_dict[ref_number-1]
             # Using regular expression to remove non-alphabetic characters at the end
             prev_ref_text = re.sub(r'[^a-zA-Z]*$', '', prev_ref_text)
             ref_text = re.sub('ibid', prev_ref_text, ref_text, flags=re.IGNORECASE)
-            #[7] CA judgment, [62].
-            #[8] Ibid, [84].
+            #[7] CA judgment, [62]
+            #[8] Ibid, [84]
             # ->
-            # [7] CA judgment, [62].
-            # [8] CA judgment, [84].
+            # [7] CA judgment, [62]
+            # [8] CA judgment, [84]
 
         reference_dict[ref_number] = ref_text
 
